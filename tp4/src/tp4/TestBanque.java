00c6fd1a228dd093294e7a8aa0c1e1aacc2c59b5
Mohamed Ait Brahim <m.aitbrahim01@gmail.com>
2026-04-17 12:28:47 +0100

    add tp4

---
+++ tp4/src/tp4/TestBanque.java
@@ -0,0 +1,48 @@
+
+package tp4;
+
+public class TestBanque {
+
+    public static void main(String[] args) {
+        Compte[] cmpt = new Compte[4];
+
+        cmpt[0] = new CompteCourant("12345", "mohamed ali", 10000, 200);
+        cmpt[1] = new CompteEpargne("89899", "mohamed ait brahim", 500, 2);
+        cmpt[2] = new ComptePremium("1234", "ismail spaksy", 700, 2000);
+        cmpt[3] = new CompteCourant("9876", "othman masteri", 4000, 6000);
+
+        
+        System.out.println(" Depot de 100   ");
+        for (Compte c : cmpt) {
+            c.deposer(100);
+            c.afficher();
+            System.out.println("\n");
+        }
+
+        
+        System.out.println("\n Retrait de 1000   ");
+        for (Compte c : cmpt) {
+            c.retirer(1000);
+            c.afficher();
+            System.out.println("\n");
+        }
+
+        
+        System.out.println("\nComptes Epargn");
+        for (Compte c : cmpt) {
+            if (c instanceof CompteEpargne) {
+                c.afficher();
+            }
+        }
+
+        System.out.println("\n Test instanceof ");
+        Compte c = new CompteEpargne("12990", "sofuane mamado", 500, 2);
+        if (c instanceof CompteCourant) {
+            
+            CompteCourant cc = (CompteCourant) c;
+            cc.afficher();
+        } else {
+            System.out.println("c n'est pas un CompteCourant");
+        }
+    }
+}
